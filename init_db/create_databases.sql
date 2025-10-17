45982224da9a9c07781c045884a6f687336121ea
Daniel241025 <06daniel.freitas@gmail.com>
2025-10-17 00:31:04 -0300

    Update: Algumas mudanças

--- init_db/create_databases.sql
+++ init_db/create_databases.sql
@@ -1,14 +1,3 @@
--- Cria bancos
-CREATE DATABASE airflow_meta;
+﻿CREATE DATABASE airflow_meta;
 CREATE DATABASE superset_meta;
 CREATE DATABASE analytics;
-
--- Cria roles (opcional)
-CREATE ROLE airflow_user WITH LOGIN PASSWORD 'postgres';
-CREATE ROLE superset_user WITH LOGIN PASSWORD 'postgres';
-CREATE ROLE analytics_user WITH LOGIN PASSWORD 'postgres';
-
--- Concede permissões
-GRANT ALL PRIVILEGES ON DATABASE airflow_meta TO airflow_user;
-GRANT ALL PRIVILEGES ON DATABASE superset_meta TO superset_user;
-GRANT ALL PRIVILEGES ON DATABASE analytics TO analytics_user;
